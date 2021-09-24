f5c9c293f010d9c5a6afbb7db4931a5b3ef93aad
progresssh <in.progress00@gmail.com>
2021-09-25 01:23:32 +0200

    Initial Repo,rudimentary structure, humanplay,draw

---
+++ script/main.js
@@ -0,0 +1,43 @@
+function computerPlay() {
+    let randNum = (Math.floor(Math.random() * 3));
+
+    switch (randNum) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
+
+function humanPlay() {
+    let choice = window.prompt("Rock, paper or scissors?", "Your choice")
+    choice = choice.toLowerCase();
+    switch (choice) {
+        case 'rock':
+            return 'Rock';
+        case 'paper':
+            return 'Paper';
+        case 'scissors':
+            return 'Scissors';
+        default:
+            alert('Please enter a correct choice: Rock Paper Scissors')
+            humanPlay();
+    }
+}
+
+const computerSelection = computerPlay();
+const humanSelection = humanPlay();
+
+function playersSelection(humanSelection, computerSelection) {
+    if (computerSelection == humanSelection) {
+        return `Draw! ${humanSelection} vs ${computerSelection}`;
+    } else if (x) {
+
+    }
+}
+
+function game() {
+
+}
